26a45c82f279cd260fdbfe31a2deef97759b558f
keneichorn <57776469+keneichorn@users.noreply.github.com>
2021-12-13 15:57:49 -0500

    Delete exercises directory

--- exercises/debugging2.rb
+++
@@ -1,7 +0,0 @@
-def shout_out_to(name)
-  name.upcase!
-
-  puts 'HEY ' + name
-end
-
-shout_out_to('you') # expected: 'HEY YOU'
--- exercises/easy5-11.rb
+++
@@ -1,7 +0,0 @@
-def spin_me(str)
-  str.split.each do |word|
-    word.reverse!
-  end.join(" ")
-end
-
-spin_me("hello world") # "olleh dlrow"
